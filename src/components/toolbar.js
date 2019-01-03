b3e02a9f1eb1a321a3cf9be69b2cfce62fc40052
Barr Covrigaru <barr@covrigaru.com>
2019-01-03 11:29:39 -0700

    initial toolbar showing

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -12,38 +12,38 @@ class Toolbar extends Component{
             unread messages
           </p>
 
-        <button className="btn btn-default">
-          <i className="fa fa-square-o"></i>
-        </button>
+          <button className="btn btn-default">
+            <i className="fa fa-square-o"></i>
+          </button>
 
-        <button className="btn btn-default" disabled="disabled">
-          Mark As Read
-        </button>
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Read
+          </button>
 
-        <button className="btn btn-default" disabled="disabled">
-          Mark As Unread
-        </button>
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Unread
+          </button>
 
-        <select className="form-control label-select" disabled="disabled">
-          <option>Apply label</option>
-          <option value="dev">dev</option>
-          <option value="personal">personal</option>
-          <option value="gschool">gschool</option>
-        </select>
+          <select className="form-control label-select" disabled="disabled">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
 
-        <select className="form-control label-select" disabled="disabled">
-          <option>Remove label</option>
-          <option value="dev">dev</option>
-          <option value="personal">personal</option>
-          <option value="gschool">gschool</option>
-        </select>
+          <select className="form-control label-select" disabled="disabled">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
 
-        <button className="btn btn-default" disabled="disabled">
-          <i className="fa fa-trash-o"></i>
-        </button>
+          <button className="btn btn-default" disabled="disabled">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
     )
   }
 }
